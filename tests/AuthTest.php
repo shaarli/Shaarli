0e96293ccb770f431ab298741efaeae7883be730
Nicolas Danelon <nicolasdanelon@gmail.com>
2015-11-25 14:48:01 -0300

    Unit framework test wip

---
+++ tests/AuthTest.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ * Auth tests
+ */
+
+// required to access $_SESSION array
+session_start();
+
+require_once 'application/Auth.php';
+
+/**
+ * Unitary tests for authentication process
+ */
+class AuthTest extends PHPUnit_Framework_TestCase
+{
+
+    /**
+     * Test testLoginState: 
+     * Valid: 
+     *
+     * @return void
+     */
+    public function testLoginStateValid($globals)
+    {
+        //
+    }
+
+    /**
+     * Test testLoginState: 
+     * Invalid: 
+     *
+     * @return void
+     */
+    public function testLoginStateInvalid($globals)
+    {
+        //
+    }
+
+    /**
+     * Test testCheckAuth: 
+     * Valid: 
+     *
+     * @return void
+     */
+    public function testCheckAuthValid($login, $password, $globals)
+    {
+        //
+    }
+
+    /**
+     * Test testCheckAuth: 
+     * Invalid: 
+     *
+     * @return void
+     */
+    public function testCheckAuthInvalid($login, $password, $globals)
+    {
+        //
+    }
+
+    /**
+     * Test testIsLoggedIn: 
+     * Valid: 
+     *
+     * @return void
+     */
+    public function testIsLoggedInValid()
+    {
+        //
+    }
+
+    /**
+     * Test testIsLoggedIn: 
+     * Invalid: 
+     *
+     * @return void
+     */
+    public function testIsLoggedInInvalid()
+    {
+        //
+    }
+
+    /**
+     * Test testLogout: 
+     * Valid: 
+     *
+     * @return void
+     */
+    public function testLogoutValid()
+    {
+        //
+    }
+
+    /**
+     * Test testLogout: 
+     * Invalid: 
+     *
+     * @return void
+     */
+    public function testLogoutInvalid()
+    {
+        //
+    }
+
+}
